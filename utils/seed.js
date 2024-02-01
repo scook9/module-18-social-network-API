25585f31a85490f1d408ac7d0138406440f24758
[Spencer Cook] <spencerrcook@hotmail.com>
2024-01-31 17:17:19 -0700

    starting seed file

---
+++ utils/seed.js
@@ -0,0 +1,24 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  //delete users collection if exists
+  let userCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
+  if (userCheck.length) {
+    await connection.dropCollection("users");
+  }
+
+  //delete thoughts collection if exists
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "thoughts" })
+    .toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection("thoughts");
+  }
+});
