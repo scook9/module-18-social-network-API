0cd2b096eac37fe509eac37e67b408ad8d8ab773
[Spencer Cook] <spencerrcook@hotmail.com>
2024-02-03 14:40:37 -0700

    adding thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,52 @@
 const Thought = require("../models/Thought");
 
-module.exports = {};
+module.exports = {
+  //get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //get one thought by _id
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //post a new thought with reactionBody and username
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //put to update user by _id
+  async updateThought(req, res) {
+    try {
+      const updatedThought = await Thought.updateOne(
+        { _id: req.params.thoughtId },
+        req.body
+      );
+      res.json(updatedThought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 //fill in import with function names from userController.js
-const {} = require("../../controllers/thoughtController");
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+} = require("../../controllers/thoughtController");
 
 //http://localhost:3001/api/thoughts
-router.route("/");
+router.route("/").get(getThoughts).post(createThought);
+
+//http://localhost:3001/api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete();
+
+//http://localhost:3001/api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").get();
 
 module.exports = router;
